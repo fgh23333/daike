5787042463b01ccb359dc268542f22df59581d2f
fgh23333 <635262140@qq.com>
2024-04-26 23:03:59 +0800

    component fix

---
+++ miniprogram/components/full-cell/full-cell.js
@@ -0,0 +1,24 @@
+// components/full_cell/full_cell.ts
+Component({
+
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+	src: 'https://onlineimg.alni.eu.org/raw/daike/my/my_about_us.png'
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
