b55056eeccf436dfaf4c5bef83e9772dc2ab028d
fgh23333 <635262140@qq.com>
2024-04-27 22:17:34 +0800

    full cell update
    full-cell组件支持两个接口，content：文本内容，src：图标地址

--- miniprogram/components/full-cell/full-cell.js
+++ miniprogram/components/full-cell/full-cell.js
@@ -1,24 +1,31 @@
-// components/full_cell/full_cell.ts
 Component({
+	/**
+	 * 组件的属性列表
+	 */
+	properties: {
+		// 接收父组件传过来的数据
+		// 属性名字必须和父组件那边的一样，和vue的prop传参类似
+		src: {
+			type: String,
+			value: ''
+		},
+		content: {
+			type: String,
+			value: ''
+		}
+	},
 
-  /**
-   * 组件的属性列表
-   */
-  properties: {
+	/**
+	 * 组件的初始数据
+	 */
+	data: {
 
-  },
+	},
 
-  /**
-   * 组件的初始数据
-   */
-  data: {
-	src: 'https://onlineimg.alni.eu.org/raw/daike/my/my_about_us.png'
-  },
+	/**
+	 * 组件的方法列表
+	 */
+	methods: {
 
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-
-  }
+	}
 })
