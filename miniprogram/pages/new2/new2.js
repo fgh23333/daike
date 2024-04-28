f255b94fa0a31d7296e7264232c22cde68d6b731
oreo234 <2026729050@qq.com>
2024-04-28 20:35:40 +0800

    表单页更新

--- miniprogram/pages/new2/new2.js
+++ miniprogram/pages/new2/new2.js
@@ -1,14 +1,26 @@
 Page({
   data: {
-    genderArray: ['男', '女', '性别不限'],
+    genders: ['男', '女', '性别不限'],
     contactArray:['微信','电话','均可'],
     monthDayTime: [[], [], ['8:15~9:50','10:05~11:40','13:00~14:35','14:45~16:20','18:00~19:35','18:00~20:30']], // 存储月、日、时间段的数据
     value: [0, 0, 0], 
     timeslots:['8:15~9:50','10:05~11:40','13:00~14:35','14:45~16:20','18:00~19:35','18:00~20:30'],
     switchValue2: true
   },
-
-
+  // 下面是性别选择
+  genderChange: function(e) {
+    const selectedGender = this.data.genders[e.detail.value];
+    this.setData({
+      selectedGender: selectedGender,
+    });
+  },
+  //以下选择联系方式
+  contactChange:function(e){
+    const selectedcontact = this.data.contactArray[e.detail.value];
+    this.setData({
+      selectedcontact: selectedcontact,
+    });
+  },
   onLoad: function () {
     this.initData();
   },
@@ -32,14 +44,14 @@ Page({
     this.setData({ 'monthDayTime[1]': days });
   },
 
-  showPicker: function () {
-    
-  },
-
   bindMultiPickerChange: function (e) {
-    console.log('picker发送选择改变，携带值为', e.detail.value)
+    const multiIndex = this.detail.value;
+    let selectedtime = this.data.monthDayTime.map((column, index) => {
+      return column[multiIndex[index]];
+    });
     this.setData({
-      value: e.detail.value
+      multiIndex: e.detail.value,
+      selectedtime:selectedtime,
     });
   },
 
