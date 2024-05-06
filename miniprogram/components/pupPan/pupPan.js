eb66666ef1771e8fdc0cc7095d14d54b89838d63
Yutian <nilei05@outlook.com>
2024-05-06 23:24:06 +0800

    跳转detail2

--- miniprogram/components/pupPan/pupPan.js
+++ miniprogram/components/pupPan/pupPan.js
@@ -1,69 +1,77 @@
 const myselfMessage = [
-	"无效操作！",
-	"你试图自己给自己代课！",
-	"不要啊！不可以！",
-	"那你快去上课吧！",
-	"不要啊！不可以！",
-	"那你快去上课吧！",
-	"不要啊！不可以！",
-	"那你快去上课吧！",
-	"aoa",
-	"www"
+  "无效操作！",
+  "你试图自己给自己代课！",
+  "不要啊！不可以！",
+  "那你快去上课吧！",
+  "不要啊！不可以！",
+  "那你快去上课吧！",
+  "不要啊！不可以！",
+  "那你快去上课吧！",
+  "aoa",
+  "www"
 ];
 
 Component({
-	/**
-	 * 组件的属性列表
-	 */
-	properties: {
-		// 接收父组件传过来的数据
-		// 属性名字必须和父组件那边的一样，和vue的prop传参类似
-		classData: {
-			type: JSON,
-			value: ''
-		}
-	},
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    // 接收父组件传过来的数据
+    // 属性名字必须和父组件那边的一样，和vue的prop传参类似
+    classData: {
+      type: JSON,
+      value: ''
+    }
+  },
 
-	/**
-	 * 组件的初始数据
-	 */
-	data: {
-    tapnumbers:0
-	},
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    tapnumbers: 0
+  },
 
-	/**
-	 * 组件的方法列表
-	 */
-	methods: {
-		dealit(){
-			if(this.data.classData.postUser === 'b787f7c3662f8ef800060bd76e2f1643'){
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    dealit() {
+      if (this.data.classData.postUser === 'b787f7c3662f8ef800060bd76e2f1643') {
         this.tapmyself()
-			}
+      }
+      else{
+        this.goTodetail2()
+      }
     },
-		getRandomInt(min, max) {
-			return Math.floor(Math.random() * (max - min + 1)) + min;
+    getRandomInt(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
     },
-    chart(){
+    chart() {
       console.log(this.data.classData)
     },
-		tapmyself(){
-			if(this.data.tapnumbers<10){
-				this.setData({
-					tapnumbers: this.data.tapnumbers+1
-				})
-			}
-			let randomInt = this.getRandomInt(1, this.data.tapnumbers);
-			wx.showToast({
-				title: myselfMessage[randomInt],
-				icon: 'none',
-				duration: 2000
-			})
-      goTodetail2: function aa() {
-        wx.navigateTo({
-          url: '/pages/detail2/detail2'
-        });
+    goTodetail2() {
+      wx.navigateTo({
+        url: `/pages/detail2/detail2?itemId=${encodeURIComponent(this.data.classData.id)}`, // 通过URL传递参数
+      });
+    },
+    tapmyself() {
+      if (this.data.tapnumbers < 10) {
+        this.setData({
+          tapnumbers: this.data.tapnumbers + 1
+        })
       }
-		}
+      let randomInt = this.getRandomInt(1, this.data.tapnumbers);
+      wx.showToast({
+        title: myselfMessage[randomInt],
+        icon: 'none',
+        duration: 2000
+      })
+      // goTodetail2: function aa() {
+      //   wx.navigateTo({
+      //     url: '/pages/detail2/detail2'
+      //   });
+      // }
+    }
 
-	}
+  }
 })
