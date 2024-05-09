a2c234fd15bea680f43e61decae6287bddc7b498
oreo234 <2026729050@qq.com>
2024-05-09 22:31:25 +0800

    111

--- miniprogram/pages/detail2/detail2.js
+++ miniprogram/pages/detail2/detail2.js
@@ -1,13 +1,33 @@
+const db = wx.cloud.database()
 Page({
-  data: {},
-  properties: {
-    
-    classData: {
-      type: JSON,
-      value: ''
+  data: {
+    classid:"",
+    classData:''
+  },
+
+  onLoad: function(options) {
+    if (options.itemId) {
+      const itemId = decodeURIComponent(options.itemId);
+      console.log('接收到的itemId:', itemId);
+      this.setData({
+        classid: itemId
+      });
+      // 根据itemId执行相应逻辑
     }
+    db.collection("orderForm").where({
+      "_id":this.data.classid
+    }).get({
+      success: (res) => {
+        this.setData({
+
+          classData : res.data[0]
+        });}
+      })
+
   },
-  onShareAppMessage() {
-    return {};
+
+  someFunction: function(event) {
+    // 自定义函数处理
   },
+  
 });
